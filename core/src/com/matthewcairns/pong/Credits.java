e6de71187125dc81b2637ae190b07ebc1549ab85
Matthew Cairns <mattrcairns@gmail.com>
2014-05-03 19:49:27 +0100

    Added credits screen.

--- core/src/com/matthewcairns/pong/Credits.java
+++ core/src/com/matthewcairns/pong/Credits.java
@@ -1,10 +1,10 @@
 package com.matthewcairns.pong;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
  * Created by Matthew Cairns on 03/05/2014.
@@ -13,7 +13,10 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class Credits implements Screen {
     final Pong game;
 
-
+    String text0 = "CREATED BY";
+    String text1 = "MATTHEW CAIRNS";
+    String text2 = "ORIGINALLY CREATED BY";
+    String text3 = "ATARI INC.";
     OrthographicCamera camera;
 
     public Credits(final Pong gam) {
@@ -32,8 +35,14 @@ public class Credits implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-
+        game.smallFont.draw(game.batch, text0, 400-game.smallFont.getBounds(text0).width/2, 400);
+        game.smallFont.draw(game.batch, text1, 400-game.smallFont.getBounds(text1).width/2, 350);
+        game.smallFont.draw(game.batch, text2, 400-game.smallFont.getBounds(text2).width/2, 150);
+        game.smallFont.draw(game.batch, text3, 400-game.smallFont.getBounds(text3).width/2, 100);
         game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
+            game.setScreen(new MainMenuScreen(game));
 
     }
 
